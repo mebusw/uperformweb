3d8ee61b81c117f291e1f2aae04510bab09ee3c4
jacky <mebusw@163.com>
2016-05-10 13:34:03 +0800

    init with template, view and tests

---
+++ uperform/test/test_trainer.py
@@ -0,0 +1,7 @@
+from django.test import TestCase
+from mock import Mock
+
+
+class TestTrainer(TestCase):
+    def test(self):
+        self.assertEquals(1, 1)
---
+++ uperform/views/__init__.py
@@ -0,0 +1,1 @@
+from view_trainer import *
