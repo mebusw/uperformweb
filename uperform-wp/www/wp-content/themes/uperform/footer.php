fdfd544136c3ebb270b8a79ab46ccfd5362ab0ec
jacky <mebusw@163.com>
2016-08-29 14:00:40 +0800

    add CNZZ stat

--- uperform-wp/www/wp-content/themes/uperform/footer.php
+++ uperform-wp/www/wp-content/themes/uperform/footer.php
@@ -83,5 +83,7 @@ Tel: (010) 85679828</p>
 
 	wp_footer();
 ?><script type="text/javascript">// alert(jQuery(window).width());</script>
+
+<script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1260239618'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s4.cnzz.com/z_stat.php%3Fid%3D1260239618%26show%3Dpic1' type='text/javascript'%3E%3C/script%3E"));</script>
 </body>
 </html>
