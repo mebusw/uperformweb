8d94f1e6b63745fa2a28319436b2e8f131b8d242
jacky <mebusw@163.com>
2016-11-05 11:48:50 +0800

    Hide auto-appended CNZZ icon

--- uperform-wp/www/wp-content/themes/uperform/footer.php
+++ uperform-wp/www/wp-content/themes/uperform/footer.php
@@ -85,5 +85,12 @@ Tel: (021) 63809913</p>
 ?><script type="text/javascript">// alert(jQuery(window).width());</script>
 
 <script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1260239618'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s4.cnzz.com/z_stat.php%3Fid%3D1260239618%26show%3Dpic1' type='text/javascript'%3E%3C/script%3E"));</script>
+
+<script type="text/javascript">
+	// Hide auto-appended CNZZ icon
+	setTimeout(function () {
+		$('img[src="http://icon.cnzz.com/img/pic1.gif"]').hide();
+	}, 2000);
+</script>
 </body>
 </html>
