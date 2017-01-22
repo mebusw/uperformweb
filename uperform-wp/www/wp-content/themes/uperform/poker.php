892e64b36348b39f5615cb57af74c66914c3dece
jacky <mebusw@163.com>
2017-01-22 14:06:05 +0800

    a online T-shirt Size Planning Poker. TODO: not link the php to main site url yet. now it's a standalone html put to /upload foler

---
+++ uperform-wp/www/wp-content/themes/uperform/poker.php
@@ -0,0 +1,51 @@
+<?php
+//get_header(); ?>    
+<!DOCTYPE html>
+<html>
+<head>
+    <title>敏捷估算计划扑克T-Shirt | UPerform Scrum CSM 认证中文培训</title>
+    <meta charset="utf-8">
+    <meta name="keywords" lang="zh-cn" content="Scrum CSM 认证 中文 培训">
+    <meta name="description" lang="zh-cn" content="Scrum CSM 认证 中文 培训">
+    <meta name="author" content="Jacky Shen">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="http://cdn.bootcss.com/lightbox2/2.9.0/css/lightbox.min.css">
+    <style type="text/css">
+        .example-image {
+            width: 7rem;
+            -webkit-border-radius: 5px;
+            -moz-border-radius: 5px;
+            -ms-border-radius: 5px;
+            -o-border-radius: 5px;
+            border-radius: 5px;
+            width: 33%;
+            float: left;
+        }
+    </style>
+</head>
+<body>
+<h1>敏捷估算计划扑克</h1>
+<div style="margin-top: 10%">
+    <a class="example-image-link" href="http://7fvfjt.com1.z0.glb.clouddn.com/poker_0.png" data-lightbox="example-set1" data-title=""><img class="example-image" src="http://7fvfjt.com1.z0.glb.clouddn.com/poker_0_thumb.png" alt="估算扑克0"/></a>
+    <a class="example-image-link" href="http://7fvfjt.com1.z0.glb.clouddn.com/poker_s.png" data-lightbox="example-set2" data-title=""><img class="example-image" src="http://7fvfjt.com1.z0.glb.clouddn.com/poker_s_thumb.png" alt="估算扑克S"/></a>
+    <a class="example-image-link" href="http://7fvfjt.com1.z0.glb.clouddn.com/poker_m.png" data-lightbox="example-set3" data-title=""><img class="example-image" src="http://7fvfjt.com1.z0.glb.clouddn.com/poker_m_thumb.png" alt="估算扑克M"/></a>
+    <a class="example-image-link" href="http://7fvfjt.com1.z0.glb.clouddn.com/poker_l.png" data-lightbox="example-set4" data-title=""><img class="example-image" src="http://7fvfjt.com1.z0.glb.clouddn.com/poker_l_thumb.png" alt="估算扑克L"/></a>
+    <a class="example-image-link" href="http://7fvfjt.com1.z0.glb.clouddn.com/poker_xl.png" data-lightbox="example-set5" data-title=""><img class="example-image" src="http://7fvfjt.com1.z0.glb.clouddn.com/poker_xl_thumb.png" alt="估算扑克XL"/></a>
+    <a class="example-image-link" href="http://7fvfjt.com1.z0.glb.clouddn.com/poker_xxl.png" data-lightbox="example-set6" data-title=""><img class="example-image" src="http://7fvfjt.com1.z0.glb.clouddn.com/poker_xxl_thumb.png" alt="估算扑克XXL"/></a>
+</div>
+<p>更多内容请访问：<a href="http://www.UPerform.cn">http://www.UPerform.CN</a><br/>
+电话：<a href="tel:+86 021-63809913">(86) 021-63809913</a><br/>
+邮箱：<a href="mailto:Service@UPerform.cn">Service@UPerform.CN</a>
+</p>
+
+<script src="http://cdn.bootcss.com/lightbox2/2.9.0/js/lightbox-plus-jquery.min.js"></script>
+<script>
+    lightbox.option({
+      'resizeDuration': 200,
+      'imageFadeDuration': 400,
+      'wrapAround': true
+    })
+</script>
+</body>
+</html>
+<?php //get_footer(); ?>
