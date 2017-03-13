e0b52da3742bd4d8f8f926f4af775ca65aa1d18b
jacky <mebusw@163.com>
2017-03-13 10:55:08 +0800

    add an action in functions.php, so to move custom pages into /theme/uperform/custom

--- uperform-wp/www/wp-content/themes/uperform/custom/poker.php
+++ uperform-wp/www/wp-content/themes/uperform/custom/poker.php
@@ -1,5 +1,4 @@
-<?php
-//get_header(); ?>    
+<?php get_header(); ?>    
 <!DOCTYPE html>
 <html>
 <head>
@@ -24,7 +23,8 @@
     </style>
 </head>
 <body>
-<h1>敏捷估算计划扑克</h1>
+<h1><b>敏捷估算计划扑克</b></h1>
+<h2>点击某张牌，即可放大打出</h2>
 <div style="margin-top: 10%">
     <a class="example-image-link" href="http://7fvfjt.com1.z0.glb.clouddn.com/poker_0.png" data-lightbox="example-set1" data-title=""><img class="example-image" src="http://7fvfjt.com1.z0.glb.clouddn.com/poker_0_thumb.png" alt="估算扑克0"/></a>
     <a class="example-image-link" href="http://7fvfjt.com1.z0.glb.clouddn.com/poker_s.png" data-lightbox="example-set2" data-title=""><img class="example-image" src="http://7fvfjt.com1.z0.glb.clouddn.com/poker_s_thumb.png" alt="估算扑克S"/></a>
@@ -33,10 +33,10 @@
     <a class="example-image-link" href="http://7fvfjt.com1.z0.glb.clouddn.com/poker_xl.png" data-lightbox="example-set5" data-title=""><img class="example-image" src="http://7fvfjt.com1.z0.glb.clouddn.com/poker_xl_thumb.png" alt="估算扑克XL"/></a>
     <a class="example-image-link" href="http://7fvfjt.com1.z0.glb.clouddn.com/poker_xxl.png" data-lightbox="example-set6" data-title=""><img class="example-image" src="http://7fvfjt.com1.z0.glb.clouddn.com/poker_xxl_thumb.png" alt="估算扑克XXL"/></a>
 </div>
-<p>更多内容请访问：<a href="http://www.UPerform.cn">http://www.UPerform.CN</a><br/>
+<!-- <p>更多内容请访问：<a href="http://www.UPerform.cn">http://www.UPerform.CN</a><br/>
 电话：<a href="tel:+86 021-63809913">(86) 021-63809913</a><br/>
 邮箱：<a href="mailto:Service@UPerform.cn">Service@UPerform.CN</a>
-</p>
+</p> -->
 
 <script src="http://cdn.bootcss.com/lightbox2/2.9.0/js/lightbox-plus-jquery.min.js"></script>
 <script>
@@ -48,4 +48,4 @@
 </script>
 </body>
 </html>
-<?php //get_footer(); ?>
+<?php get_footer(); ?>
