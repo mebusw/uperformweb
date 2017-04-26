b73aa3508f6e81faed7c2398225ce5d4d0a93f4b
jacky <mebusw@163.com>
2017-04-26 15:29:05 +0800

    update layout and links for planning poker on WeChat

--- uperform-wp/www/wp-content/themes/uperform/custom/poker.php
+++ uperform-wp/www/wp-content/themes/uperform/custom/poker.php
@@ -23,15 +23,17 @@
     </style>
 </head>
 <body>
+<div style="margin-top: 100px;margin-bottom: 100px;    min-height: 400px;">
 <h1><b>敏捷估算计划扑克</b></h1>
 <h2>点击某张牌，即可放大打出</h2>
 <div style="margin-top: 10%, margin-bottom: 10%">
-    <a class="example-image-link" href="http://7fvfjt.com1.z0.glb.clouddn.com/poker_0.png" data-lightbox="example-set1" data-title=""><img class="example-image" src="http://7fvfjt.com1.z0.glb.clouddn.com/poker_0_thumb.png" alt="估算扑克0"/></a>
-    <a class="example-image-link" href="http://7fvfjt.com1.z0.glb.clouddn.com/poker_s.png" data-lightbox="example-set2" data-title=""><img class="example-image" src="http://7fvfjt.com1.z0.glb.clouddn.com/poker_s_thumb.png" alt="估算扑克S"/></a>
-    <a class="example-image-link" href="http://7fvfjt.com1.z0.glb.clouddn.com/poker_m.png" data-lightbox="example-set3" data-title=""><img class="example-image" src="http://7fvfjt.com1.z0.glb.clouddn.com/poker_m_thumb.png" alt="估算扑克M"/></a>
-    <a class="example-image-link" href="http://7fvfjt.com1.z0.glb.clouddn.com/poker_l.png" data-lightbox="example-set4" data-title=""><img class="example-image" src="http://7fvfjt.com1.z0.glb.clouddn.com/poker_l_thumb.png" alt="估算扑克L"/></a>
-    <a class="example-image-link" href="http://7fvfjt.com1.z0.glb.clouddn.com/poker_xl.png" data-lightbox="example-set5" data-title=""><img class="example-image" src="http://7fvfjt.com1.z0.glb.clouddn.com/poker_xl_thumb.png" alt="估算扑克XL"/></a>
-    <a class="example-image-link" href="http://7fvfjt.com1.z0.glb.clouddn.com/poker_xxl.png" data-lightbox="example-set6" data-title=""><img class="example-image" src="http://7fvfjt.com1.z0.glb.clouddn.com/poker_xxl_thumb.png" alt="估算扑克XXL"/></a>
+    <a class="example-image-link" href="http://app-17sports.qiniudn.com/poker_0.png" data-lightbox="example-set1" data-title=""><img class="example-image" src="http://app-17sports.qiniudn.com/poker_0_thumb.png" alt="估算扑克0"/></a>
+    <a class="example-image-link" href="http://app-17sports.qiniudn.com/poker_s.png" data-lightbox="example-set2" data-title=""><img class="example-image" src="http://app-17sports.qiniudn.com/poker_s_thumb.png" alt="估算扑克S"/></a>
+    <a class="example-image-link" href="http://app-17sports.qiniudn.com/poker_m.png" data-lightbox="example-set3" data-title=""><img class="example-image" src="http://app-17sports.qiniudn.com/poker_m_thumb.png" alt="估算扑克M"/></a>
+    <a class="example-image-link" href="http://app-17sports.qiniudn.com/poker_l.png" data-lightbox="example-set4" data-title=""><img class="example-image" src="http://app-17sports.qiniudn.com/poker_l_thumb.png" alt="估算扑克L"/></a>
+    <a class="example-image-link" href="http://app-17sports.qiniudn.com/poker_xl.png" data-lightbox="example-set5" data-title=""><img class="example-image" src="http://app-17sports.qiniudn.com/poker_xl_thumb.png" alt="估算扑克XL"/></a>
+    <a class="example-image-link" href="http://app-17sports.qiniudn.com/poker_xxl.png" data-lightbox="example-set6" data-title=""><img class="example-image" src="http://app-17sports.qiniudn.com/poker_xxl_thumb.png" alt="估算扑克XXL"/></a>
+</div>
 </div>
 <!-- <p>更多内容请访问：<a href="http://www.UPerform.cn">http://www.UPerform.CN</a><br/>
 电话：<a href="tel:+86 021-63809913">(86) 021-63809913</a><br/>
