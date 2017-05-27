24e934dbdb159e32b4564446727de1e6d4c74f1e
jacky <mebusw@163.com>
2017-05-27 11:44:48 +0800

    recover "public class" should show all courses

--- uperform-wp/www/wp-content/themes/uperform/category-class.php
+++ uperform-wp/www/wp-content/themes/uperform/category-class.php
@@ -40,41 +40,24 @@
 							<span class="left-section" style="font-size: ">找不到你的城市？请致电<a href="tel:021-34753688">021-34753688</a></span>
 						</div>
 					</div>
-            <div class="right-col right-section course-list container">
-                <h2>近期敏捷课程公开班</h2>
-                <?php if (have_posts()) : ?>
-                <ul class="row">
-                <?php $special_query = new WP_Query('category_name=special&showposts=8');
-                while ($special_query->have_posts()) : $special_query->the_post();
-                $do_not_duplicate = $post->ID; ?>
-                    <li class="col-md-4" <?php post_class() ?> id="post-<?php the_ID(); ?>">
-                        <div>
-                            <h4 class="course-title width-auto"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h4>
-                            <span class="course-title-bg"></span>
-                            <div class="clear"></div>
-                        </div>
-                        <div class="entry">
-                        <?php 
-                            global $more;    // Declare global $more (before the loop).
-                            $more = 0;       // Set (inside the loop) to display content above the more tag.
-                            the_content("详细介绍");
-                        ?>
-                        </div>
-                    </li>
-                  <?php endwhile; ?>
-                  </ul>   
-                <?php else : ?>
-                        <h2 class="center">Not Found</h2>
-                        <p class="center">Sorry, but you are looking for something that isn't here.</p>
-                        <?php get_search_form(); ?>
+					<div class="course-list class-list container">
+						<h2>敏捷课程表</h2>
+						<?php
+						$category_description = category_description();
+						if ( ! empty( $category_description ) )
+						echo '<div class="archive-meta">' . $category_description . '</div>';
 
-                <?php endif; ?>
-                <div class="more">
-                    <a class="faq-link" href="/faq#q1">哪个课程适合我?</a>
-                    <a class="more-btn" href="/category/courses/class">所有公开班</a><span class="more-btn-bg"></span>
-                    <div class="clear"></div>
-                </div>
-            </div>
+						/* Run the loop for the category page to output the posts.
+						* If you want to overload this in a child theme then include a file
+						* called loop-category.php and that will be used instead.
+						*/
+						get_template_part( 'loop', 'category' );
+						?>
+					</div>
+					<div class="clear"></div>
+				</div>
+				<div class="clear"></div>
+			</div>
 		</div><!-- #content -->
 	</div><!-- #container -->
 <?php get_footer(); ?>
