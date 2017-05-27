5bf0542c37c8ef114e420eb9f1e09b2225c61957
jacky <mebusw@163.com>
2017-05-27 11:16:00 +0800

    update looking and result of "recent public course"

--- uperform-wp/www/wp-content/themes/uperform/category-activities.php
+++ uperform-wp/www/wp-content/themes/uperform/category-activities.php
@@ -82,41 +82,40 @@
 		<?php endif; ?>
 				</div>
 			</div>
-			<div class="right-col right-section course-list">
-				<h2>近期敏捷课程公开班</h2>
-				<?php if (have_posts()) : ?>
-  <?php $special_query = new WP_Query('category_name=special&showposts=4');
-  while ($special_query->have_posts()) : $special_query->the_post();
-  $do_not_duplicate = $post->ID; ?>
-    <!-- Do stuff... -->
-				<ul>
-					<li <?php post_class() ?> id="post-<?php the_ID(); ?>">
-						<div>
-							<h3 class="course-title width-auto"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3><span class="course-title-bg"></span>
-							<div class="clear"></div>
-						</div>
-						<div class="entry">
-							<?php 
-global $more;    // Declare global $more (before the loop).
-$more = 0;       // Set (inside the loop) to display content above the more tag.
-the_content("详细介绍");
-?>
-						</div>
-					</li>
-				</ul>	
-  <?php endwhile; ?>
-	<?php else : ?>
+            <div class="right-col right-section course-list container">
+                <h2>近期敏捷课程公开班</h2>
+                <?php if (have_posts()) : ?>
+                <ul class="row">
+                <?php $special_query = new WP_Query('category_name=special&showposts=8');
+                while ($special_query->have_posts()) : $special_query->the_post();
+                $do_not_duplicate = $post->ID; ?>
+                    <li class="col-md-4" <?php post_class() ?> id="post-<?php the_ID(); ?>">
+                        <div>
+                            <h4 class="course-title width-auto"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h4>
+                            <span class="course-title-bg"></span>
+                            <div class="clear"></div>
+                        </div>
+                        <div class="entry">
+                        <?php 
+                            global $more;    // Declare global $more (before the loop).
+                            $more = 0;       // Set (inside the loop) to display content above the more tag.
+                            the_content("详细介绍");
+                        ?>
+                        </div>
+                    </li>
+                  <?php endwhile; ?>
+                  </ul>   
+                <?php else : ?>
+                        <h2 class="center">Not Found</h2>
+                        <p class="center">Sorry, but you are looking for something that isn't here.</p>
+                        <?php get_search_form(); ?>
 
-		<h2 class="center">Not Found</h2>
-		<p class="center">Sorry, but you are looking for something that isn't here.</p>
-		<?php get_search_form(); ?>
-
-	<?php endif; ?>
-	<div class="more">
-					<a class="faq-link" href="/faq#q1">哪个课程适合我?</a>
-					<a class="more-btn" href="/category/courses/class">所有公开班</a><span class="more-btn-bg"></span>
-					<div class="clear"></div>
-				</div>
-			</div>
-		</div>
+                <?php endif; ?>
+                <div class="more">
+                    <a class="faq-link" href="/faq#q1">哪个课程适合我?</a>
+                    <a class="more-btn" href="/category/courses/class">所有公开班</a><span class="more-btn-bg"></span>
+                    <div class="clear"></div>
+                </div>
+            </div>
+	</div>
 <?php get_footer(); ?>
